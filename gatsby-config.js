da5ae0a1008888da125eda87b48b6f5fa42c73fc
simplexologylab <ramsey@simplexologylab.com>
2018-12-19 22:29:42 -0500

    title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
-    description: 'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
-    author: '@gatsbyjs',
+    title: 'Simplexology Lab',
+    description: 'We strive to make the complex; simple',
+    author: '@simplexologylab',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
