bb7a2896c250c118f2a77f8920a397d96a33ca41
simplexologylab <ramsey@simplexologylab.com>
2019-02-19 22:24:39 -0500

    group cards + nav

--- src/templates/group-posts.js
+++ src/templates/group-posts.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { graphql, Link, navigate } from "gatsby"
+import { graphql, navigate } from "gatsby"
 import Img from 'gatsby-image';
 import styled from 'styled-components';
 
@@ -15,16 +15,26 @@ const TitleHeading = styled(Heading)`
 `;
 
 const PostCard = ({title, description, image, slug}) => (
-  <Box background="dark-1" round>
-    <Box>{title}</Box>
-    {image && <Img fluid={image} />}
-    <Box>{description}</Box>
-    <Button label="open" onClick={() => navigate(slug)}/>
-  </Box>
+  <Button onClick={() => navigate(slug)}>
+      <Box background="light-2" round elevation="medium">
+        <Heading margin="small" size="small">{title}</Heading>
+        <Box flex>{image && <Img fluid={image} />}</Box>
+        <Paragraph margin="none">{description}</Paragraph>
+      </Box>
+  </Button>
 )
 
 class GroupPostsTemplate extends React.Component {
-    render(){
+  resizeGrid = (size) => {
+    switch(size) {
+      case 'medium': return {count: 2, size: "auto"}
+      case 'large': return {count: 3, size: "auto"}
+      case 'xlarge': return {count: 4, size: "auto"}
+      default: return "auto"
+    }
+  }  
+  
+  render(){
         const pageInfo = this.props.data.mdx;
         const subPages = this.props.data.allMdx.edges;
         const siteTitle = this.props.data.site.siteMetadata.title;
@@ -33,74 +43,46 @@ class GroupPostsTemplate extends React.Component {
             <Grommet>
               <Box>
                 <Box direction="row" margin="xsmall">
-                  <Button plain label="projects" icon={<LinkPrevious />} onClick={() => navigate('/projects')} />
+                  <Button plain label="back" icon={<LinkPrevious />} onClick={() => navigate('/projects')} />
                   <Box align="end" jusify="end" margin="xsmall" fill>
                     <SiteHeading plain label={siteTitle} onClick={() => navigate(`/`)} />
                     <TitleHeading level='1' size="small" margin="small">{pageInfo.frontmatter.title}</TitleHeading>                  
                   </Box>
                 </Box>
               </Box>
-              <Box pad={{"horizontal": "xlarge"}}>
                 <ResponsiveContext.Consumer>
                   { size => (
-                    <Grid
-                      columns={["1/2", "1/2"]}
-                      gap={size}
-                    >
-                      {subPages.map(page => {
-                        if(page.node.frontmatter.image) {
+                    <Box pad={size}>
+                      <Grid
+                        columns={this.resizeGrid(size)}
+                        gap="medium"
+                      >
+                        {subPages.map(page => {
+                          if(page.node.frontmatter.image) {
+                            return (
+                              <PostCard
+                                key={page.id}
+                                title={page.node.frontmatter.title}
+                                description={page.node.frontmatter.description}
+                                image={page.node.frontmatter.image.childImageSharp.fluid}
+                                slug={page.node.fields.slug}
+                              />
+                            )
+                          }
                           return (
-                            <PostCard
+                            <PostCard 
                               key={page.id}
-                              title={page.node.fields.slug}
-                              description={page.node.excerpt}
-                              image={page.node.frontmatter.image.childImageSharp.fluid}
+                              title={page.node.frontmatter.title}
+                              description={page.node.frontmatter.description}
                               slug={page.node.fields.slug}
                             />
                           )
+                        })
                         }
-                        return (
-                          <PostCard 
-                            key={page.id}
-                            title={page.node.fields.slug}
-                            description={page.node.excerpt}
-                            slug={page.node.fields.slug}
-                          />
-                        )
-                      })
-                      }
-                    </Grid>
+                      </Grid>
+                    </Box>
                   )}
                 </ResponsiveContext.Consumer>
-              
-                {/* {subPages.map(page => {
-                    if(page.node.frontmatter.image) {
-                        return (
-                            <Link to={page.node.fields.slug} id={page.node.id}>
-                              <Box direction="row-responsive" fill="horizontal">
-                                <Box pad="small" flex size="small">
-                                  <Img fluid={page.node.frontmatter.image.childImageSharp.fluid} />
-                                </Box>
-                                <Box justify="center" pad="small" background="neutral-1" round elevation="large">
-                                  <Heading margin="xsmall">{page.node.frontmatter.title}</Heading>
-                                  <Paragraph>{page.node.excerpt}</Paragraph>
-                                  <Button label="Open" />
-                                </Box>
-                              </Box>
-                            </Link>
-                        )
-                    }
-                    return (
-                      <Link to={page.node.fields.slug}>
-                        <Box background="dark-1" align="center" pad="small" round margin="small" elevation="large">
-                          <Heading margin="xsmall">{page.node.frontmatter.title}</Heading>
-                          <Paragraph textAlign="center">{page.node.excerpt}</Paragraph>
-                          <Button label="Open" />
-                        </Box>
-                      </Link>
-                    )
-                })} */}
-              </Box>
             </Grommet>
         )
     } 
@@ -125,9 +107,9 @@ query PostsBySlug($slug: String!) {
       edges {
         node {
           id
-          excerpt(pruneLength: 160)
           frontmatter{
             title
+            description
             image {
                 childImageSharp {
                     fluid(maxWidth:600) {
@@ -146,9 +128,9 @@ query PostsBySlug($slug: String!) {
         fields: {slug: { eq: $slug}}
     ){
         id
-        excerpt(pruneLength: 160)
         frontmatter {
             title
+            description
             date(formatString: "MMMM DD, YYYY")
         }
     }
--- src/templates/post.js
+++ src/templates/post.js
@@ -1,22 +1,35 @@
 import React from "react"
-import { Link, graphql } from "gatsby"
+import { navigate, graphql } from "gatsby"
 import MDXRenderer from "gatsby-mdx/mdx-renderer";
 import { Grommet, Box, Button, Heading, ResponsiveContext } from 'grommet';
 import { LinkPrevious } from 'grommet-icons';
 import styled from 'styled-components';
 
-import Bio from "../components/bio"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import { rhythm, scale } from "../utils/typography"
+const SiteHeading = styled(Button)`
+  font-family: 'Rajdhani';
+`;
+
+const TitleHeading = styled(Heading)`
+  font-family: 'Shadows Into Light';
+`;
 
 class PostTemplate extends React.Component {
   render() {
     const post = this.props.data.mdx
     const siteTitle = this.props.data.site.siteMetadata.title
     const { previous, next } = this.props.pageContext
+    console.log('props', window.history);
     return (
       <Grommet>
+        <Box>
+          <Box direction="row" margin="xsmall">
+            <Button plain label="back" icon={<LinkPrevious />} onClick={() => window.history.back()} />
+            <Box align="end" jusify="end" margin="xsmall" fill>
+              <SiteHeading plain label={siteTitle} onClick={() => navigate(`/`)} />
+              <TitleHeading level='1' size="small" margin="small">{post.frontmatter.title}</TitleHeading>                  
+            </Box>
+          </Box>
+        </Box>
         <ResponsiveContext.Consumer>
           { size => (
             <Box>
@@ -26,54 +39,6 @@ class PostTemplate extends React.Component {
           )}
         </ResponsiveContext.Consumer>
       </Grommet>
-      
-      
-      // <Layout location={this.props.location} title={siteTitle}>
-      //   <SEO title={post.frontmatter.title} description={post.excerpt} />
-      //   <h1>{post.frontmatter.title}</h1>
-      //   <p
-      //     style={{
-      //       ...scale(-1 / 5),
-      //       display: `block`,
-      //       marginBottom: rhythm(1),
-      //       marginTop: rhythm(-1),
-      //     }}
-      //   >
-      //     {post.frontmatter.date}
-      //   </p>
-      //   <MDXRendrer>{post.node.code.body}</MDXRendrer>
-      //   <hr
-      //     style={{
-      //       marginBottom: rhythm(1),
-      //     }}
-      //   />
-      //   <Bio />
-
-      //   <ul
-      //     style={{
-      //       display: `flex`,
-      //       flexWrap: `wrap`,
-      //       justifyContent: `space-between`,
-      //       listStyle: `none`,
-      //       padding: 0,
-      //     }}
-      //   >
-      //     <li>
-      //       {previous && (
-      //         <Link to={previous.fields.slug} rel="prev">
-      //           {previous.frontmatter.title}
-      //         </Link>
-      //       )}
-      //     </li>
-      //     <li>
-      //       {next && (
-      //         <Link to={next.fields.slug} rel="next">
-      //           {next.frontmatter.title} →
-      //         </Link>
-      //       )}
-      //     </li>
-      //   </ul>
-      // </Layout>
     )
   }
 }
@@ -90,7 +55,6 @@ export const pageQuery = graphql`
     }
     mdx(fields: { slug: { eq: $slug } }) {
       id
-      excerpt(pruneLength: 160)
       frontmatter {
         title
         date(formatString: "MMMM DD, YYYY")
