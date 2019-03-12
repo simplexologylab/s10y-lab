7261622321356d0c5e2de34b7f931a20f5b5e7ea
simplexologylab <ramsey@simplexologylab.com>
2019-03-12 16:43:11 -0400

    added apollo-client

--- src/api/client.js
+++ src/api/client.js
@@ -1,23 +1,25 @@
+// https://github.com/awslabs/aws-mobile-appsync-sdk-js/blob/master/README.md
+
 import AWSAppSyncClient from 'aws-appsync';
 import fetch from 'isomorphic-fetch';
 
 export const client = new AWSAppSyncClient({
   url: `https://kzg5zidnovh73om2b3jrlcboqm.appsync-api.us-east-2.amazonaws.com/graphql`,
   region: `us-east-2`,
-    auth: {
-        type: "API_KEY",
-        apiKey: "da2-ruqgy2vnenhsdcqtpfb2r4qm3m"
-    },
-    offlineConfig: {
-        callback: (err, succ) => {
-            if (err) {
-                const { mutation, variables } = err;
-                console.warn(`Error for ${mutation}`, err, variables);
-            } else {
-                const { mutation, variables } = succ;
-                console.info(`SUCCESS for ${mutation}`, succ, variables);
-            }
-        }
-    },
-    fetch
+  auth: {
+      type: "API_KEY",
+      apiKey: "da2-ruqgy2vnenhsdcqtpfb2r4qm3m"
+  },
+  offlineConfig: {
+      callback: (err, succ) => {
+          if (err) {
+              const { mutation, variables } = err;
+              console.warn(`Error for ${mutation}`, err, variables);
+          } else {
+              const { mutation, variables } = succ;
+              console.info(`SUCCESS for ${mutation}`, succ, variables);
+          }
+      }
+  },
+  fetch
 });
