3d97146949ae739cd7e1b3d6791828efa27250e8
simplexologylab <ramsey@simplexologylab.com>
2019-03-12 16:24:45 -0400

    added rehydrated thing

--- src/api/wrap-root-element.js
+++ src/api/wrap-root-element.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import { ApolloProvider } from 'react-apollo';
+import { Rehydrated } from 'aws-appsync-react';
 
 import { client } from './client';
 
 export const wrapRootElement = ({ element }) => (
-    <ApolloProvider client={client}>{element}</ApolloProvider>
+    <ApolloProvider client={client}>
+      <Rehydrated>
+        {element}
+      </Rehydrated>
+    </ApolloProvider>
 )
