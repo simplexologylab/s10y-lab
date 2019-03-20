e960201ba01237ef3cab5d92d84c894332cb12a0
simplexologylab <ramsey@simplexologylab.com>
2019-03-19 23:51:44 -0400

    added prettier

--- src/templates/post-stack.js
+++ src/templates/post-stack.js
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
-import { graphql, navigate } from "gatsby";
-import MDXRenderer from "gatsby-mdx/mdx-renderer";
+import React, { useState } from 'react';
+import { graphql, navigate } from 'gatsby';
+import MDXRenderer from 'gatsby-mdx/mdx-renderer';
 
-import { Grommet, Anchor, Layer, Box, Grid, Button, Text, Heading, ResponsiveContext} from 'grommet';
+import { Grommet, Anchor, Layer, Box, Grid, Button, Text, Heading, ResponsiveContext } from 'grommet';
 import { Menu, LinkPrevious, EmptyCircle } from 'grommet-icons';
 import styled from 'styled-components';
 
@@ -13,11 +13,7 @@ const FixedButton = styled(Button)`
   z-index: 100;
 `;
 
-const AnchorBox = ({text}) => (
-  <Box background="dark-1">
-    {text}
-  </Box>
-)
+const AnchorBox = ({ text }) => <Box background="dark-1">{text}</Box>;
 
 const SiteHeading = styled(Button)`
   font-family: 'Rajdhani';
@@ -27,7 +23,7 @@ const TitleHeading = styled(Heading)`
   font-family: 'Shadows Into Light';
 `;
 
-export default (props) => {
+export default props => {
   const pageInfo = props.data.mdx;
   const subPages = props.data.allMdx.edges;
   const siteTitle = props.data.site.siteMetadata.title;
@@ -43,7 +39,9 @@ export default (props) => {
               <Button basis="auto" plain label="back" icon={<LinkPrevious />} onClick={() => navigate('/projects')} />
               <Box align="end" jusify="end" margin="xsmall" fill>
                 <SiteHeading plain label={siteTitle} onClick={() => navigate(`/`)} />
-                <TitleHeading level='1' size="small" margin="small">{pageInfo.frontmatter.title}</TitleHeading>                  
+                <TitleHeading level="1" size="small" margin="small">
+                  {pageInfo.frontmatter.title}
+                </TitleHeading>
               </Box>
             </Box>
             <Box>
@@ -56,54 +54,63 @@ export default (props) => {
                 ))}
               </Box>
             </Box>
-            { menu ? (
-                  <Layer full="vertical" position="left" margin="10px"
-                    onClickOutside={() => setMenu(false)} onEsc={() => setMenu(false)}
-                  >
-                    <Box as="header" direction="row" elevation="medium" align="center" justify="center">
-                      <Heading level={3} margin="small">{pageInfo.frontmatter.title}</Heading>
-                    </Box>
-                    <Box flex overflow="auto" pad="xsmall">
-                      {subPages.map(page => (
-                        <Anchor href={`#${page.node.id}`} label={<Box direction="row" gap="medium" pad={{ "vertical": "medium", "horizontal": "xsmall"}}>
-                        <EmptyCircle />{page.node.frontmatter.title}</Box>} />
-                      ))}
-                    </Box>
-                    <Box
-                      as="footer"
-                      border={{ side: "top"}}
-                      pad="small"
-                      align="center"
-                    >
-                      <FixedButton primary label="Close" onClick={() => setMenu(false)} />
-                    </Box>
-                  </Layer>
-                ) : (
-                  <Box animation="fadeIn" alignSelf="center">
-                    <FixedButton icon={<Menu style={{ verticalAlign: "bottom" }} />} primary color="accent-4" justify="stretch" onClick={() => setMenu(true)} />
-                  </Box>
-                )}
+            {menu ? (
+              <Layer
+                full="vertical"
+                position="left"
+                margin="10px"
+                onClickOutside={() => setMenu(false)}
+                onEsc={() => setMenu(false)}
+              >
+                <Box as="header" direction="row" elevation="medium" align="center" justify="center">
+                  <Heading level={3} margin="small">
+                    {pageInfo.frontmatter.title}
+                  </Heading>
+                </Box>
+                <Box flex overflow="auto" pad="xsmall">
+                  {subPages.map(page => (
+                    <Anchor
+                      href={`#${page.node.id}`}
+                      label={
+                        <Box direction="row" gap="medium" pad={{ vertical: 'medium', horizontal: 'xsmall' }}>
+                          <EmptyCircle />
+                          {page.node.frontmatter.title}
+                        </Box>
+                      }
+                    />
+                  ))}
+                </Box>
+                <Box as="footer" border={{ side: 'top' }} pad="small" align="center">
+                  <FixedButton primary label="Close" onClick={() => setMenu(false)} />
+                </Box>
+              </Layer>
+            ) : (
+              <Box animation="fadeIn" alignSelf="center">
+                <FixedButton
+                  icon={<Menu style={{ verticalAlign: 'bottom' }} />}
+                  primary
+                  color="accent-4"
+                  justify="stretch"
+                  onClick={() => setMenu(true)}
+                />
+              </Box>
+            )}
           </Box>
         )}
       </ResponsiveContext.Consumer>
     </Grommet>
-  )
-}
+  );
+};
 
 export const PostStackBySlugQuery = graphql`
-query PostsStackBySlug($slug: String!) {
+  query PostsStackBySlug($slug: String!) {
     site {
       siteMetadata {
         title
         author
       }
     }
-    allFile(
-      filter:{
-        relativePath: {regex: $slug}
-        extension: {eq: "json"}
-      }
-    ){
+    allFile(filter: { relativePath: { regex: $slug }, extension: { eq: "json" } }) {
       totalCount
       edges {
         node {
@@ -119,39 +126,34 @@ query PostsStackBySlug($slug: String!) {
       }
     }
     allMdx(
-      filter:{
-        fileAbsolutePath: {regex: $slug},
-        fields: { slug: {ne: $slug}}
-      },
-      sort: {fields: frontmatter___order, order: ASC}
-    ){
+      filter: { fileAbsolutePath: { regex: $slug }, fields: { slug: { ne: $slug } } }
+      sort: { fields: frontmatter___order, order: ASC }
+    ) {
       edges {
         node {
           id
           code {
             body
           }
-          frontmatter{
+          frontmatter {
             title
             description
             order
             image {
               childImageSharp {
-                  fluid(maxWidth:600) {
-                      ...GatsbyImageSharpFluid
-                  }
+                fluid(maxWidth: 600) {
+                  ...GatsbyImageSharpFluid
                 }
               }
             }
-            fields {
-              slug
-            }
+          }
+          fields {
+            slug
           }
         }
       }
-    mdx(
-        fields: {slug: { eq: $slug}}
-    ){
+    }
+    mdx(fields: { slug: { eq: $slug } }) {
       id
       frontmatter {
         title
@@ -160,8 +162,8 @@ query PostsStackBySlug($slug: String!) {
         date(formatString: "MMMM DD, YYYY")
         image {
           childImageSharp {
-            fluid(maxWidth:600) {
-                ...GatsbyImageSharpFluid
+            fluid(maxWidth: 600) {
+              ...GatsbyImageSharpFluid
             }
           }
         }
@@ -171,4 +173,4 @@ query PostsStackBySlug($slug: String!) {
       }
     }
   }
-`
+`;
