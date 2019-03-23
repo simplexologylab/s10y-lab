20c3ceb9bce4294c1214802ed2da3216fd92751e
simplexologylab <ramsey@simplexologylab.com>
2019-03-23 00:33:53 -0400

    login/logout button

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,7 @@
 // custom typefaces
 import "typeface-montserrat"
 import "typeface-merriweather"
-import Amplify, { Auth } from 'aws-amplify';
+import Amplify from 'aws-amplify';
 import awsConfig from './src/aws-exports';
 
 Amplify.configure(awsConfig);
--- src/pages/add-auth.js
+++ src/pages/add-auth.js
@@ -1,15 +1,13 @@
-import React from 'react'
-import { withAuthenticator } from 'aws-amplify-react'
+import React from 'react';
+import { Link } from 'gatsby';
+import { withAuthenticator } from 'aws-amplify-react';
 
 const Auth = props => (
   <div>
     <h1>Should only see this when logged in</h1>
-    {props.authState === 'signedIn' && (
-      <div>show me</div>
-    )}
+    <Link to="/">Home</Link>
     <pre>{JSON.stringify(props, 0, 2)}</pre>
-
   </div>
-)
+);
 
-export default withAuthenticator(Auth, true)
+export default withAuthenticator(Auth, true);
