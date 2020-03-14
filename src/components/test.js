1ab8befc7e9a7a3d6ff1f2c4ad0e312b19883385
simplexologylab <ramsey@simplexologylab.com>
2020-03-14 11:19:41 -0400

    test

--- src/components/test.js
+++ src/components/test.js
@@ -4,7 +4,7 @@ import Img from 'gatsby-image';
 class Test extends React.Component {
     render() {
         return (
-            <div>this is my test</div>
+            <div>this is my test...</div>
         )
     }
 }
